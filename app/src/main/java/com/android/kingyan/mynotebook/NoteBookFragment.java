70a4ad35a4c8d34243fb65391959ef98d1ceb7bb
King Yan <mcse2003.yan@gmail.com>
2016-04-01 17:10:12 +0800

    update

---
+++ app/src/main/java/com/android/kingyan/mynotebook/NoteBookFragment.java
@@ -0,0 +1,66 @@
+package com.android.kingyan.mynotebook;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+/**
+ * Created by yanj on 16/04/01.
+ */
+public class NoteBookFragment extends Fragment {
+    private Note mNote;
+    private EditText mTitleEditText;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNote = new Note();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_notebook, container, false);
+        mTitleEditText = (EditText) v.findViewById(R.id.note_title);
+        mTitleEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                mNote.setmTitle(s.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mDateButton = (Button) v.findViewById(R.id.button_date);
+        mDateButton.setText(mNote.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.checkbox_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mNote.setSolved(isChecked);
+            }
+        });
+        return v;
+    }
+}
