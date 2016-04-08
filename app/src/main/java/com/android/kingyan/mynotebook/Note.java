9f3154e48ff3dc9315c81dc55cc26d5734ce7b87
King Yan <mcse2003.yan@gmail.com>
2016-04-08 21:27:15 +0800

    update

--- app/src/main/java/com/android/kingyan/mynotebook/Note.java
+++ app/src/main/java/com/android/kingyan/mynotebook/Note.java
@@ -1,5 +1,8 @@
 package com.android.kingyan.mynotebook;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,6 +10,10 @@ import java.util.UUID;
  * Created by yanj on 16/03/31.
  */
 public class Note {
+    public static final String JSON_ID = "id";
+    public static final String JSON_TITLE = "title";
+    public static final String JSON_DATE = "date";
+    public static final String JSON_SOLVED = "solved";
     private UUID mId;
     private String mTitle;
     private Date mDate;
@@ -15,6 +22,24 @@ public class Note {
     public Note() {
         this.mId = UUID.randomUUID();
         mDate = new Date();
+    }
+
+    public Note(JSONObject json) throws JSONException {
+        mId = UUID.fromString(json.getString(JSON_ID));
+        if (json.has(JSON_TITLE)) {
+            mTitle = json.getString(JSON_TITLE);
+        }
+        mDate = new Date(json.getLong(JSON_DATE));
+        mSolved = json.getBoolean(JSON_SOLVED);
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_DATE, mDate.getTime());
+        json.put(JSON_SOLVED, mSolved);
+        return json;
     }
 
     public UUID getmId() {
--- app/src/main/java/com/android/kingyan/mynotebook/NoteLab.java
+++ app/src/main/java/com/android/kingyan/mynotebook/NoteLab.java
@@ -1,6 +1,7 @@
 package com.android.kingyan.mynotebook;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -9,13 +10,23 @@ import java.util.UUID;
  * Created by yanj on 16/04/01.
  */
 public class NoteLab {
+    private static final String TAG = "NoteLab";
+    private static final String FILENAME = "mynotebook.json";
     private static NoteLab sNoteLab;
     private Context mContext;
     private ArrayList<Note> mNotes;
+    private NoteBookJSONSerializer mNoteBookJSONSerializer;
 
     public NoteLab(Context context) {
         mContext = context;
-        mNotes = new ArrayList<Note>();
+        mNoteBookJSONSerializer = new NoteBookJSONSerializer(mContext, FILENAME);
+        try {
+            mNotes = mNoteBookJSONSerializer.getLoadNotes();
+            Log.d(TAG, "load success");
+        } catch (Exception e) {
+            mNotes = new ArrayList();
+            Log.d(TAG, "load notes fail");
+        }
     }
 
     public static NoteLab get(Context context) {
@@ -23,6 +34,17 @@ public class NoteLab {
             sNoteLab = new NoteLab(context.getApplicationContext());
         }
         return sNoteLab;
+    }
+
+    public boolean saveNotes() {
+        try {
+            mNoteBookJSONSerializer.saveNotes(mNotes);
+            Log.d(TAG, "notes saved");
+            return true;
+        } catch (Exception e) {
+            Log.d(TAG, "notes saved fail");
+            return false;
+        }
     }
 
     public ArrayList<Note> getNotes() {
